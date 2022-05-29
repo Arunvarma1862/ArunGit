53a6c13f782278cad9a51709909eb5479306b331
arunbabu120894@gmail.com <arunbabu120894@gmail.com>
2022-05-29 22:18:21 +0530

    first commit

--- src/testngproject/ind.java
+++ src/testngproject/ind.java
@@ -3,7 +3,13 @@ package testngproject;
 public class ind {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stubs
+		System.out.println("hello world");
+		System.out.println("hello world");
+		System.out.println("hello world");
+		System.out.println("hello world");
+		System.out.println("hello world");
+		System.out.println("hello world");
 
 	}
 
